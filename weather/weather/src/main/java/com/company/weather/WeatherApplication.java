90e2d12177d6932beae70ff92508b5f63a9f6f39
Ilqar Eskerov <ilqar.ilqar.eskerov.eskerov@mail.ru>
2023-03-27 03:22:09 +0400

    Swagger

--- weather/weather/src/main/java/com/company/weather/WeatherApplication.java
+++ weather/weather/src/main/java/com/company/weather/WeatherApplication.java
@@ -1,7 +1,10 @@
 package com.company.weather;
 
+import com.company.weather.model.WeatherEntity;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.yaml.snakeyaml.comments.CommentLine;
 
 @SpringBootApplication
 public class WeatherApplication {
@@ -9,5 +12,4 @@ public class WeatherApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
 	}
-
 }
